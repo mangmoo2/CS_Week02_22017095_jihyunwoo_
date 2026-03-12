1fb7cbe10599749accf38f8c64e1c23a1d63d391
mangmoo2 <jihw0522@gmail.com>
2026-03-12 11:14:56 +0900

    점수를 계산하여 표시하기, 게임이 점점 어려워지도록 만들기

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,11 @@
 using System.Media;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace CatchButton
 {
     public partial class Form1 : Form
     {
-        
+        int score = 0; //점수 변수
         public Form1()
         {
             InitializeComponent();
@@ -14,18 +15,22 @@ namespace CatchButton
         {
 
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             SystemSounds.Beep.Play(); // 2_1. 소리 재생
             MessageBox.Show("축하합니다~!");
+
+            score += 100;     // 잡으면 점수 증가
+            button1.Width = (int)(button1.Width * 0.9); // 잡으면 넓이 10% 줄어들기
+            button1.Height = (int)(button1.Height * 0.9); // 잡으면 높이 10% 줄어들기
+            Update_(); // 점수 업데이트
         }
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
             // 1. 난수생성기
             Random rd= new Random();
-            // 2. 가용영역계산_버튼이폼테두리에걸리지않게보호
+            // 2. 가용영역계산_버튼이폼테두리에걸리지않게보호    
             int maxX= this.ClientSize.Width;int maxY= this.ClientSize.Height;
             // 3. 랜덤좌표추출
             int nextX= rd.Next(0, maxX);int nextY= rd.Next(0, maxY);
@@ -36,6 +41,14 @@ namespace CatchButton
 
             // 2_1. 도망갈 때의 효과음 재생
             SystemSounds.Asterisk.Play();
+            Update_();// 점수 업데이트
+
+            score -= 10; //놓차면 10점 감점
+        }
+
+        private void Update_() // 업데이트   
+        { 
+            this.Text = $"점수: {score}";
         }
     }
 }
