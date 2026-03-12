b0634df757f8349bffb18076d044a7ee5979f6e1
user <user@DESKTOP-519IA7K>
2026-03-12 10:15:16 +0900

    버튼 컨트롤 영역안으로 마우스가들어가면 버튼을 랜덤위치로 이동초기 커밋.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,39 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(215, 127);
+            button1.Name = "button1";
+            button1.Size = new Size(223, 92);
+            button1.TabIndex = 0;
+            button1.Text = "나를 잡아봐라!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,29 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            // 1. 난수생성기
+            Random rd= new Random();
+            // 2. 가용영역계산_버튼이폼테두리에걸리지않게보호
+            int maxX= this.ClientSize.Width;int maxY= this.ClientSize.Height;
+            // 3. 랜덤좌표추출
+            int nextX= rd.Next(0, maxX);int nextY= rd.Next(0, maxY);
+            // 4. 위치할당
+            button1.Location= new Point(nextX, nextY);
+            // 5. 시각적피드백
+            this.Text= $"버튼위치: ({nextX}, {nextY})";
+        }
     }
 }
